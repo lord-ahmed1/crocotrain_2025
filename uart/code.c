5022dd997cb5ec61fd7431f093297debe01414f6
lord ahmed <lordahmed1612@gmail>
2025-08-08 19:11:06 +0200

    new code uart

--- uart/code.c
+++ uart/code.c
@@ -4,55 +4,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void UART_init(unsigned long USART_BAUDRATE)
+void configuration(){
+  
+
+UCSR0B=0b00011000;//enable transmit and recieve
+  
+  
+UCSR0C=0b00000110;//asynchronous no parity one stop bit with 8 bit data 
+  
+//9600 baud rate associated with ubrr of 103 which is 0000 01100111 , ubbrh is four most significant bits 0000 , ubrr0l is 8 least significant bits 01100111 , 
+UBRR0H=0b0000;
+UBRR0L=0b01100111;
+  
+}
+//UDR0=0b00100001;
+void transmit(char data){
+if (  ( (UCSR0A & 0b00100000)==0b00100000 ) ) //executes if Bit 5 USART Data Register Empty is one
 {
-    unsigned int BAUD_PRESCALE = ((F_CPU / (USART_BAUDRATE * 16UL)) - 1);
+  data+=1;
+UDR0=data;
+  UDR0='\n';
 
-    // Set baud rate
-    UBRR0L = (uint8_t)BAUD_PRESCALE;
-    UBRR0H = (uint8_t)(BAUD_PRESCALE >> 8);
-
-    // Enable transmitter and receiver
-    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
-
-    // Set frame format: 8 data bits, 1 stop bit
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
-unsigned char UART_RxChar()
-{
-    while (!(UCSR0A & (1 << RXC0)));
-    return UDR0;
+ 
 }
 
-void UART_TxChar(char ch)
-{
-    while (!(UCSR0A & (1 << UDRE0)));
-    UDR0 = ch;
+char recieve(){
+  while ( !  ( (UCSR0A & 0b10000000)==0b10000000 )   );  //loops if Bit 7 USART Receive Complete is 0
+return UDR0;
+  
+  
 }
 
-void UART_SendString(char *str)
-{
-    while (*str)
-    {
-        UART_TxChar(*str++);
-    }
-}
 
 int main()
 {
-    char c;
-    UART_init(9600);
 
-    UART_SendString("\n\rEcho Test Ready:\n\r");
-
-    while (1)
-    {
-        c = UART_RxChar();     // Wait and receive
-        UART_TxChar(c+1);        // Echo it back
-    	UART_TxChar('\n'); 
-    }
-
+configuration();
+ 
+  while (1){
+  char data=recieve();
+  transmit(data);
+  }
+  
     return 0;
 }
+
 
